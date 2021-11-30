da5638b9cea528d01f7d898ade4895f8f3e603c0
HyoTaek-Jang <hyotaek9812@gmail.com>
2021-11-30 20:33:57 +0900

    feat: 1:n 소켓 테스팅

--- src/main/java/com/forkfork/cake/socket/WebSocketHandler.java
+++ src/main/java/com/forkfork/cake/socket/WebSocketHandler.java
@@ -1,6 +1,11 @@
 package com.forkfork.cake.socket;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -8,12 +13,44 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 @Component
 public class WebSocketHandler extends TextWebSocketHandler {
 
+	private List<WebSocketSession> sessionList = new ArrayList<>();
+
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+
+		sessionList.add(session);
+
+		for(WebSocketSession s : sessionList) {
+			s.sendMessage(new TextMessage(session.getId() + "님이 입장하셨습니다."));
+		}
+	}
+
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-		String payload = message.getPayload();
-		System.out.println("payload = " + payload);
 
-		TextMessage initialGreeting = new TextMessage("Welcome to Swoomi Chat Server ~O_O~");
-		session.sendMessage(initialGreeting);
+		for(WebSocketSession s : sessionList) {
+			s.sendMessage(new TextMessage(session.getId() + ":" + message.getPayload()));
+		}
 	}
+
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+
+
+		sessionList.remove(session);
+
+		for(WebSocketSession s : sessionList) {
+			s.sendMessage(new TextMessage(session.getId() + "님이 퇴장하셨습니다."));
+		}
+	}
+	// @Override
+	// protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+	// 	String payload = message.getPayload();
+	// 	System.out.println("payload = " + payload);
+	// 	System.out.println("session = " + session.getId());
+	// 	System.out.println("session = " + session.getUri());
+	//
+	// 	TextMessage initialGreeting = new TextMessage("Welcome to Swoomi Chat Server ~O_O~");
+	// 	session.sendMessage(initialGreeting);
+	// }
 }
