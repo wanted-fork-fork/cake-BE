0aa220510e8ba2fd8816c4294be71d8669c00f0b
HyoTaek-Jang <hyotaek9812@gmail.com>
2022-01-02 16:55:06 +0900

    feat: chat관련 테이블 구성 - joinColumn 추가

--- src/main/java/com/forkfork/cake/domain/ChatMsg.java
+++ src/main/java/com/forkfork/cake/domain/ChatMsg.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -34,9 +35,11 @@ public class ChatMsg {
 	String msg;
 
 	@ManyToOne
+	@JoinColumn
 	User user;
 
 	@ManyToOne
+	@JoinColumn
 	ChatRoom chatRoom;
 
 }
--- src/main/java/com/forkfork/cake/domain/ChatRoomMember.java
+++ src/main/java/com/forkfork/cake/domain/ChatRoomMember.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import lombok.AllArgsConstructor;
@@ -23,9 +24,11 @@ public class ChatRoomMember {
 	private Long id;
 
 	@ManyToOne
+	@JoinColumn
 	User user;
 
 	@ManyToOne
+	@JoinColumn
 	ChatRoom chatRoom;
 
 }
