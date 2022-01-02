8b84d01af526708090f0cf871007b482d6a27b22
HyoTaek-Jang <hyotaek9812@gmail.com>
2022-01-02 17:29:32 +0900

    feat: study save시, chatRoom, RoomMember 추가

--- src/main/java/com/forkfork/cake/controller/socket/RoomController.java
+++ src/main/java/com/forkfork/cake/controller/socket/RoomController.java
@@ -1,59 +1,59 @@
-package com.forkfork.cake.controller.socket;
-
-import java.util.List;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import com.forkfork.cake.domain.ChatRoom;
-import com.forkfork.cake.dto.socket.ChatRoomDTO;
-import com.forkfork.cake.repository.ChatRoomRepository;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
-
-@Controller
-@RequiredArgsConstructor
-@RequestMapping(value = "/chat")
-@Log4j2
-public class RoomController {
-
-	private final ChatRoomRepository chatRoomRepository;
-
-	@GetMapping("/room")
-	public String rooms(Model model) {
-		return "/chat/room";
-	}
-
-	@GetMapping("/rooms")
-	@ResponseBody
-	public List<ChatRoomDTO> room() {
-		return chatRoomRepository.findAllRooms();
-	}
-
-	@PostMapping("/room")
-	@ResponseBody
-	public ChatRoomDTO createRoom(@RequestParam String name) {
-		return chatRoomRepository.createChatRoomDTO(name);
-	}
-
-	@GetMapping("/room/enter/{roomId}")
-	public String roomDetail(Model model, @PathVariable String roomId) {
-		model.addAttribute("roomId", roomId);
-		return "/chat/roomdetail";
-	}
-
-	@GetMapping("/room/{roomId}")
-	@ResponseBody
-	public ChatRoomDTO roomInfo(@PathVariable String roomId) {
-		return chatRoomRepository.findRoomById(roomId);
-	}
-}
+// package com.forkfork.cake.controller.socket;
+//
+// import java.util.List;
+//
+// import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.ResponseBody;
+// import org.springframework.web.servlet.ModelAndView;
+// import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+//
+// import com.forkfork.cake.domain.ChatRoom;
+// import com.forkfork.cake.dto.socket.ChatRoomDTO;
+// import com.forkfork.cake.repository.ChatRoomRepository;
+//
+// import lombok.RequiredArgsConstructor;
+// import lombok.extern.log4j.Log4j2;
+//
+// @Controller
+// @RequiredArgsConstructor
+// @RequestMapping(value = "/chat")
+// @Log4j2
+// public class RoomController {
+//
+// 	private final ChatRoomRepository chatRoomRepository;
+//
+// 	@GetMapping("/room")
+// 	public String rooms(Model model) {
+// 		return "/chat/room";
+// 	}
+//
+// 	@GetMapping("/rooms")
+// 	@ResponseBody
+// 	public List<ChatRoomDTO> room() {
+// 		return chatRoomRepository.findAllRooms();
+// 	}
+//
+// 	@PostMapping("/room")
+// 	@ResponseBody
+// 	public ChatRoomDTO createRoom(@RequestParam String name) {
+// 		return chatRoomRepository.createChatRoomDTO(name);
+// 	}
+//
+// 	@GetMapping("/room/enter/{roomId}")
+// 	public String roomDetail(Model model, @PathVariable String roomId) {
+// 		model.addAttribute("roomId", roomId);
+// 		return "/chat/roomdetail";
+// 	}
+//
+// 	@GetMapping("/room/{roomId}")
+// 	@ResponseBody
+// 	public ChatRoomDTO roomInfo(@PathVariable String roomId) {
+// 		return chatRoomRepository.findRoomById(roomId);
+// 	}
+// }
--- src/main/java/com/forkfork/cake/service/ChatRoomService.java
+++ src/main/java/com/forkfork/cake/service/ChatRoomService.java
@@ -3,6 +3,7 @@ package com.forkfork.cake.service;
 import org.springframework.stereotype.Service;
 
 import com.forkfork.cake.domain.ChatRoom;
+import com.forkfork.cake.domain.ChatRoomMember;
 import com.forkfork.cake.domain.Study;
 import com.forkfork.cake.domain.User;
 import com.forkfork.cake.repository.ChatRoomMemberRepository;
@@ -18,7 +19,12 @@ public class ChatRoomService {
 	private final ChatRoomMemberRepository chatRoomMemberRepository;
 
 	public ChatRoom saveChatRoom(User user, Study study) {
-		// return chatRoomRepository.save(chatRoom);
+		ChatRoom chatRoom = chatRoomRepository.save(ChatRoom.builder().study(study).build());
+
+		ChatRoomMember chatRoomMember = ChatRoomMember.builder().user(user).chatRoom(chatRoom).build();
+		chatRoomMemberRepository.save(chatRoomMember);
+
+		return chatRoom;
 	}
 
 }
